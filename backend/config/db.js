0898835bc951bd32a35a7b5b09094fd36d07d454
stemG <77492060+gkana19@users.noreply.github.com>
2023-05-18 00:46:20 +0700

    update database name

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      dbName: "Secure-Chat-Web"
+      dbName: process.env.DB_NAME,
     });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
